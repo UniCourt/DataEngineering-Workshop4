208e108d03f6f99b783f1c783f5c7d730145ff88
Prajwal&Amin <prajwal.ur2002@gmail.com>
2023-10-06 23:10:05 +0530

    workshop4_home_work

---
+++ Vivekananda_session4_4VP20CS062_Prajwal/Batman/members/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('students/<int:rolno>', views.StudentView.as_view()),
+    path('students/', views.StudentView.as_view()),
+    path('students/<str:branch>', views.StudentView.as_view()),
+    path('start_python_blog_scraping', views.python_blog_scrap, name='triger'),
+    path('rest/blog/', views.BlogView.as_view()),
+    path('python_blog_scraping/<int:job_id>', views.python_blog_scraping, name="scraping"),
+    ]
