e53b8bd227728d23760d673eab305cc0467bb0e0
Ankita Hegde <ankita@Ubuntu.myguest.virtualbox.org>
2023-10-29 19:07:25 +0530

    assignment of session4

---
+++ myworld1/members/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('rest/student/<int:rolno>', views.StudentView.as_view()),
+    path('rest/student/', views.StudentView.as_view()),
+    path('rest/student/<str:branch>', views.StudentView.as_view()),
+    path('rest/student/<int:rolno>', views.StudentView.as_view()),
+    path('rest/student/', views.StudentView.as_view()),
+    path('rest/student/<str:branch>', views.StudentView.as_view()),
+    path('start_python_blog_scraping', views.python_blog_scrap, name='triger'),
+    path('rest/student/<int:rolno>', views.StudentView.as_view()),
+    path('rest/student/', views.StudentView.as_view()),
+    path('rest/student/<str:branch>', views.StudentView.as_view()),
+    path('start_python_blog_scraping', views.python_blog_scrap, name='triger'),
+    path('rest/blog/', views.BlogView.as_view()),
+    path('python_blog_scraping/<int:job_id>', views.python_blog_scraping, name="scraping")
+]
+
